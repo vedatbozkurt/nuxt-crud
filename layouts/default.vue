020d98c7044f4094f3d541108466442bd4433b1d
Vedat Bozkurt <info@wedat.org>
2021-08-24 18:29:46 +0300

    default layout created

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<!--
+ * @Author: @vedatbozkurt
+ * @Email: info@wedat.org
+ * @Date: 2021-08-24 17:30:36
+ * @LastEditors: @vedatbozkurt
+ * @LastEditTime: 2021-08-24 17:30:36
+-->
+<template>
+    <div>
+        üst
+        <nuxt />
+        alt
+    </div>
+</template>
