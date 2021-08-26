d597cfa5913811ff2d742d4d8ce7d71aef0e20b0
Vedat Bozkurt <info@wedat.org>
2021-08-26 15:00:54 +0300

    init user ibans pages

---
+++ pages/ibans/_id/edit.vue
@@ -0,0 +1,7 @@
+<!--
+ * @Author: @vedatbozkurt
+ * @Email: info@wedat.org
+ * @Date: 2021-08-26 15:00:21
+ * @LastEditors: @vedatbozkurt
+ * @LastEditTime: 2021-08-26 15:00:21
+-->
---
+++ pages/ibans/index.vue
@@ -0,0 +1,7 @@
+<!--
+ * @Author: @vedatbozkurt
+ * @Email: info@wedat.org
+ * @Date: 2021-08-26 14:58:41
+ * @LastEditors: @vedatbozkurt
+ * @LastEditTime: 2021-08-26 14:58:42
+-->
