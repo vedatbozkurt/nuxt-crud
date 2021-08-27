d222e5e0b35f6d336a25bc51cdbe014cd0967b64
Vedat Bozkurt <info@wedat.org>
2021-08-27 14:47:50 +0300

    todos page separated

--- pages/index.vue
+++ pages/index.vue
@@ -3,103 +3,11 @@
  * @Email: info@wedat.org
  * @Date: 2021-08-24 14:26:45
  * @LastEditors: @vedatbozkurt
- * @LastEditTime: 2021-08-27 00:43:37
+ * @LastEditTime: 2021-08-27 14:46:46
 -->
 <template>
   <div class="container">
-    <div
-      class="
-        h-100
-        w-full
-        flex
-        items-center
-        justify-center
-        bg-teal-lightest
-        font-sans
-      "
-    >
-      <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
-        <div class="mb-4">
-          <h1 class="text-grey-darkest">Todo List</h1>
-          <div class="flex mt-4">
-            <input
-              v-model="new_todo"
-              class="
-                shadow
-                appearance-none
-                border
-                rounded
-                w-full
-                py-2
-                px-3
-                mr-4
-                text-grey-darker
-              "
-              placeholder="Add Todo"
-            />
-            <button
-              @click="addTodo()"
-              class="
-                flex-no-shrink
-                p-2
-                border-2
-                rounded
-                text-teal
-                border-teal
-                hover:text-white
-                hover:bg-teal
-              "
-            >
-              Add
-            </button>
-          </div>
-        </div>
-        <div>
-          <div
-            class="flex mb-4 items-center"
-            v-for="todo in todoList"
-            :key="todo.id"
-          >
-            <p class="w-full text-grey-darkest">
-              {{ todo.title }}
-            </p>
-            <button
-              class="
-                flex-no-shrink
-                p-2
-                ml-4
-                mr-2
-                border-2
-                rounded
-                hover:text-white
-                text-green
-                border-green
-                hover:bg-green
-              "
-            >
-              Done
-            </button>
-            <button
-            @click="removeTodo(todo.id)"
-              class="
-                flex-no-shrink
-                bg-indigo-500
-                p-2
-                ml-2
-                border-2
-                rounded
-                text-red
-                border-red
-                hover:text-white
-                hover:bg-red
-              "
-            >
-              Remove
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <p>Menuden işlem yapabilirsiniz.</p>
   </div>
 </template>
 
@@ -107,29 +15,13 @@
 export default {
   data() {
     return {
-      new_todo: "yeni not",
     };
   },
   computed: {
-    todoList() {
-      return this.$store.getters["todo/todoList"];
-      // return this.$store.state.todo.todoList;
-    },
   },
   methods: {
-    addTodo() {
-      let tempData = {
-        id: this.todoList.length + 1,
-        title: this.new_todo,
-      };
-      return this.$store.dispatch("todo/addTodo", tempData);
-    },
-    removeTodo(id) {
-      return this.$store.dispatch("todo/removeTodo", id);
-    },
   },
   created() {
-    this.$store.dispatch("todo/getTodoList");
   }
 };
 </script>
---
+++ pages/todos/index.vue
@@ -0,0 +1,135 @@
+<!--
+ * @Author: @vedatbozkurt
+ * @Email: info@wedat.org
+ * @Date: 2021-08-24 14:26:45
+ * @LastEditors: @vedatbozkurt
+ * @LastEditTime: 2021-08-27 00:43:37
+-->
+<template>
+  <div class="container">
+    <div
+      class="
+        h-100
+        w-full
+        flex
+        items-center
+        justify-center
+        bg-teal-lightest
+        font-sans
+      "
+    >
+      <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+        <div class="mb-4">
+          <h1 class="text-grey-darkest">Todo List</h1>
+          <div class="flex mt-4">
+            <input
+              v-model="new_todo"
+              class="
+                shadow
+                appearance-none
+                border
+                rounded
+                w-full
+                py-2
+                px-3
+                mr-4
+                text-grey-darker
+              "
+              placeholder="Add Todo"
+            />
+            <button
+              @click="addTodo()"
+              class="
+                flex-no-shrink
+                p-2
+                border-2
+                rounded
+                text-teal
+                border-teal
+                hover:text-white
+                hover:bg-teal
+              "
+            >
+              Add
+            </button>
+          </div>
+        </div>
+        <div>
+          <div
+            class="flex mb-4 items-center"
+            v-for="todo in todoList"
+            :key="todo.id"
+          >
+            <p class="w-full text-grey-darkest">
+              {{ todo.title }}
+            </p>
+            <button
+              class="
+                flex-no-shrink
+                p-2
+                ml-4
+                mr-2
+                border-2
+                rounded
+                hover:text-white
+                text-green
+                border-green
+                hover:bg-green
+              "
+            >
+              Done
+            </button>
+            <button
+            @click="removeTodo(todo.id)"
+              class="
+                flex-no-shrink
+                bg-indigo-500
+                p-2
+                ml-2
+                border-2
+                rounded
+                text-red
+                border-red
+                hover:text-white
+                hover:bg-red
+              "
+            >
+              Remove
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      new_todo: "yeni not",
+    };
+  },
+  computed: {
+    todoList() {
+      return this.$store.getters["todo/todoList"];
+      // return this.$store.state.todo.todoList;
+    },
+  },
+  methods: {
+    addTodo() {
+      let tempData = {
+        id: this.todoList.length + 1,
+        title: this.new_todo,
+      };
+      return this.$store.dispatch("todo/addTodo", tempData);
+    },
+    removeTodo(id) {
+      return this.$store.dispatch("todo/removeTodo", id);
+    },
+  },
+  created() {
+    this.$store.dispatch("todo/getTodoList");
+  }
+};
+</script>
